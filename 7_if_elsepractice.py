54b4b9d209264d2c37ca5e9867201bcfd0b535c9
lyfofnikesh <ny0062036@gmail.com>
2026-09-07 15:14:57 +0530

    Add files via upload

--- 7_if_elsepractice.py
+++ 7_if_elsepractice.py
@@ -112,3 +112,15 @@
 # case 2:
 #  print("tuesday")
 
+# a = int(input("enter your number: "))
+# b = int(input("enter your number: "))
+# c = int(input("enter your number: "))
+
+# if a > b and a > c:
+#   print("a")
+
+# elif b > a and b > c:
+#   print("b")
+
+# else:
+#   print("c")
